a1b1e9a1cc5de09e062ac5cef0471822abb18cd5
POO <jair.vega@epn.edu.ec>
2024-05-23 09:49:29 -0500

    Cambio

--- src/Herencia/Persona.java
+++ src/Herencia/Persona.java
@@ -1,5 +1,16 @@
 package Herencia;
 
 public class Persona {
+    String nombre;
+    public Persona(String nombre){
+        this.nombre=nombre;
+    }
 
+    public void Info() {
+        System.out.println("Nombre: "+nombre);
+    }
+    public static void main (String[] args){
+        Persona persona = new Persona("Juan");
+        persona.Info();
+    }
 }
