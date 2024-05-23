3dab26f3e9db53ad68eb9f0a269b7bed33495779
POO <jair.vega@epn.edu.ec>
2024-05-23 10:03:44 -0500

    Nuevo

--- src/Herencia/Persona.java
+++ src/Herencia/Persona.java
@@ -2,15 +2,17 @@ package Herencia;
 //EL padre hererda a la hija y la hija hereda a la hija
 public class Persona {
     String nombre;
-    public Persona(String nombre){
+    int edad;
+    public Persona(String nombre, int edad){
         this.nombre=nombre;
+        this.edad = edad;
     }
-
     public void Info() {
         System.out.println("Nombre: "+nombre);
+        System.out.println("Edad: "+edad);
     }
     public static void main (String[] args){
-        Persona persona = new Persona("Juan");
+        Persona persona = new Persona("Juan", 15);
         persona.Info();
     }
 }
